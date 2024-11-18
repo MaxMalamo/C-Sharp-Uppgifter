34dda6b6d7861f69ac0c4a5cd344311413232b88
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-18 13:54:22 +0200

    Add files via upload

---
+++ 1-VariablesInC#.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Variables_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Max";
+            int number = 3;
+            int secondNumber = 5;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("Second Number: " + secondNumber);
+
+        }
+    }
+}
