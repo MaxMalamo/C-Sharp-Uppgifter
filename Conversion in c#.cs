5aaec1ba27b8e172aa6d435e92a2719f40e48274
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-19 08:52:45 +0200

    Add files via upload

---
+++ Conversion in c#.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberString = "333";
+            int number = int.Parse(numberString);
+            Console.WriteLine(number);
+        }
+    }
+}
