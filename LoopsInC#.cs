7cf1a910451f11c22d82bb0d36dedceb3549d721
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-20 12:49:26 +0200

    Add files via upload

---
+++ LoopsInC#.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many times do you want the loop to run?: ");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            char loopCharacter = 'X';
+            int startNumber = 1;
+
+            if (userChoice >= 0)
+            {
+                for (int i = startNumber; i <= userChoice; i++)
+                {
+                    Console.WriteLine(loopCharacter);
+                }
+                Console.WriteLine("Your loop has finished!");
+            }
+            else
+            {
+                Console.WriteLine("Enter a number above 0");
+            }
+        }
+    }
+}
