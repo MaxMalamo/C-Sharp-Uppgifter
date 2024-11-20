86ab0e6130e35b83e9ca9e3d98f988dc7be3d222
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-20 12:56:09 +0200

    Add files via upload

---
+++ WhileLoopsInC#.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopsInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 5)
+            {
+                counter++;
+                Console.WriteLine(counter.ToString("Counter"));
+            }
+            Console.WriteLine("The loop has finished!");
+        }
+    }
+}
