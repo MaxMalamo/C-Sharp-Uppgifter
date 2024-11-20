868c452aebd29704fcf24a64a59527c8cbbb1737
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-20 12:23:50 +0200

    Add files via upload

---
+++ If-Statements-In-C#.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements_In_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+            int ageVote = Convert.ToInt32(age);
+
+            if (age >= 18)
+                {
+                    Console.WriteLine("You are old enough to vote!");
+                }
+            else
+                {
+                    Console.WriteLine("You can't vote yet!");
+                }
+             
+
+
+
+        }
+    }
+}
