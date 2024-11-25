e9cc5032d55ca642720f7893e236d390d3938c13
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-25 12:59:25 +0200

    Add files via upload

---
+++ MessagePrinterTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePrinterTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message: ");
+            var userMessage = Console.ReadLine();
+
+            char[] messageArray = userMessage.ToCharArray();
+            Array.Reverse(messageArray);
+
+            Console.WriteLine("Original order: " + userMessage);
+            Console.WriteLine("Reverse order: " + messageArray);
+
+            foreach (char c in userMessage)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine("-------------");
+
+            foreach (char c in messageArray)
+            {
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
