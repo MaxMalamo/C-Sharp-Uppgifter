fccb95cf1854a1e73bd03f3d193ebcefa624a2b4
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-25 13:15:25 +0200

    Add files via upload

---
+++ PasswordCheckerInC#.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordCheckerInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a password: ");
+            string firstPass = Console.ReadLine();
+            Console.WriteLine("Enter it again: ");
+            string secondPass = Console.ReadLine(); 
+
+            if (string.IsNullOrEmpty(firstPass) || string.IsNullOrEmpty(secondPass))
+            {
+                Console.WriteLine("Please enter a password");
+            }
+
+            if (firstPass == secondPass)
+            {
+                Console.WriteLine("Passwords match");
+            }
+            else
+            {
+                Console.WriteLine("Passwords do not match");
+            }
+
+
+
+
+        }
+    }
+}
