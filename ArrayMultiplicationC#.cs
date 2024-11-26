6c1f4ddcb6c505ffeffc615cfcdf3372c4f1ca9b
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-26 10:52:05 +0200

    Add files via upload

---
+++ ArrayMultiplicationC#.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMultiplicationCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            int num = 7;
+            int length = 5;
+
+            
+            int[] multiplicationArray = new int[length];
+
+            Console.WriteLine($"Multiplication Table for {num}:");
+
+            
+            for (int i = 0; i < length; i++)
+            {
+                multiplicationArray[i] = num * (i + 1); 
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{num} x {i + 1} = {multiplicationArray[i]}");
+            }
+        }
+    }
+}
