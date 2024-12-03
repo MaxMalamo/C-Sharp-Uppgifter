73ef05c6e15fd10e033308077a484a3f4429cb1e
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-12-03 08:54:41 +0200

    Add files via upload

---
+++ AreaCalculationTaskInC#.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculationTaskInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width: ");
+            int Width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the height: ");
+            int Height = Convert.ToInt32(Console.ReadLine());
+            
+            int area = Calculation(Width, Height);
+            Console.WriteLine($"The area of the triangle is {area}");
+
+        }
+
+        static int Calculation(int Width, int Height)
+        {
+            return Width * Height / 2;
+        }
+    }
+}
