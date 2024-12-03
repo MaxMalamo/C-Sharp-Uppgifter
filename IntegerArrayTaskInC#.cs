42c0d5e602e776e83d9c35be52fec5b1d0214a13
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-12-03 09:27:11 +0200

    Add files via upload

---
+++ IntegerArrayTaskInC#.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerArrayTaskInC_
+{
+    class numbers
+    {
+        public static void SumOfNumbers()
+        {
+            int[] arrayNumbers = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+           
+            foreach (int number in arrayNumbers)
+            {
+                sum  += number;
+            }                    
+
+            Console.WriteLine("The length of the array is " + arrayNumbers.Length + " numbers");
+
+            if (sum > -1)
+            {
+                Console.WriteLine("The total is: " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Cannot add up an empty array");
+            }
+
+        }
+      
+        
+    }
+
+     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            numbers.SumOfNumbers();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
